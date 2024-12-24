9badf012d32818fd1d4ee26f231bf8fcc06cee43
Khushi <khushikalankar@gmail.com>
2024-12-24 20:22:25 +0530

    Removed submodule and added backend as a regular folder

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import { Hono } from "hono";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+app.use('/*', cors());
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
+
+
+
+
+export default app;
